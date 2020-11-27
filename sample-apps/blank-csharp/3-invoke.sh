8b88861c9fe81f879eafd3041ce376d2910c7294
Danilo Resende <danilo.isr@gmail.com>
2020-11-27 17:49:23 -0300

    customize the aws default command

--- sample-apps/blank-csharp/3-invoke.sh
+++ sample-apps/blank-csharp/3-invoke.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -eo pipefail
-FUNCTION=$(aws cloudformation describe-stack-resource --stack-name blank-csharp --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
+# set -eo pipefail
+FUNCTION=$(aws --profile my-lambda cloudformation describe-stack-resource --stack-name danilo-blank-csharp --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
 
 while true; do
-  aws lambda invoke --function-name $FUNCTION --payload file://event.json out.json
+  aws --profile my-lambda lambda invoke --function-name $FUNCTION --cli-binary-format raw-in-base64-out --payload file://event.json out.json
   cat out.json
   echo ""
   sleep 2
