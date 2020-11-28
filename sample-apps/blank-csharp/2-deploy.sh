739626e9262f308e8ade6ae6c430afc4aa12f4fa
Danilo Resende <danilo.isr@gmail.com>
2020-11-27 23:54:53 -0300

    remove aws profile from commands

--- sample-apps/blank-csharp/2-deploy.sh
+++ sample-apps/blank-csharp/2-deploy.sh
@@ -4,5 +4,5 @@ ARTIFACT_BUCKET=$(cat bucket-name.txt)
 cd src/blank-csharp
 dotnet lambda package
 cd ../../
-aws --profile my-lambda cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
-aws --profile my-lambda cloudformation deploy --template-file out.yml --stack-name danilo-blank-csharp --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation package --template-file template.yml --s3-bucket $ARTIFACT_BUCKET --output-template-file out.yml
+aws cloudformation deploy --template-file out.yml --stack-name danilo-blank-csharp --capabilities CAPABILITY_NAMED_IAM
--- sample-apps/blank-csharp/3-invoke.sh
+++ sample-apps/blank-csharp/3-invoke.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # set -eo pipefail
-FUNCTION=$(aws --profile my-lambda cloudformation describe-stack-resource --stack-name danilo-blank-csharp --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
+FUNCTION=$(aws cloudformation describe-stack-resource --stack-name danilo-blank-csharp --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
 
 while true; do
-  aws --profile my-lambda lambda invoke --function-name $FUNCTION --cli-binary-format raw-in-base64-out --payload file://event.json out.json
+  aws lambda invoke --function-name $FUNCTION
   cat out.json
   echo ""
   sleep 2
